d96038f402ae920a6028d54bb556dc40318f79e4
algadaix <alexandre@qured.com>
2020-12-09 23:40:46 +0000

    IIFE

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
 'use strict';
-console.log('Hello world');
-const videoEl = document.getElementById('video');
-const btn = document.getElementById('btn');
+(function () {
+	console.log('Hello world');
+	const videoEl = document.getElementById('video');
+	const btn = document.getElementById('btn');
 
-// Promt to select media stream, pass to video element, then play
-async function selectMediaStream() {
-	try {
-		const mediaStream = await navigator.mediaDevices.getDisplayMedia();
-		console.log(mediaStream);
-		videoEl.srcObject = mediaStream;
-		videoEl.onloadedmetadata = () => {
-			videoEl.play();
-		};
-	} catch (error) {
-		//catch error here
-		console.log(error);
-		throw error;
+	// Promt to select media stream, pass to video element, then play
+	async function selectMediaStream() {
+		try {
+			const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+			console.log(mediaStream);
+			videoEl.srcObject = mediaStream;
+			videoEl.onloadedmetadata = () => {
+				videoEl.play();
+			};
+		} catch (error) {
+			//catch error here
+			console.log(error);
+			throw error;
+		}
 	}
-}
 
-btn.addEventListener('click', async () => {
-	//disable
-	btn.disabled = true;
-	// start picture to picture
-	await videoEl.requestPictureInPicture();
-	btn.disabled = false;
-});
-// On load
-selectMediaStream();
+	btn.addEventListener('click', async () => {
+		//disable
+		btn.disabled = true;
+		// start picture to picture
+		await videoEl.requestPictureInPicture();
+		btn.disabled = false;
+	});
+	// On load
+	selectMediaStream();
+})();
